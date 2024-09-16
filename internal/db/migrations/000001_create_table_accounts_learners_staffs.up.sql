8778be94c954006e017a9ea6343c7e18de0e6b8b
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2024-09-17 02:14:26 +0700

    🧐: seed data for schools

--- internal/db/migrations/000001_create_table_accounts_learners_staffs.up.sql
+++ internal/db/migrations/000001_create_table_accounts_learners_staffs.up.sql
@@ -1,4 +1,4 @@
-CREATE table accounts(
+CREATE table accounts (
     id uuid PRIMARY KEY,
     first_name character varying (10) NOT NULL,
     last_name character varying(10) NOT NULL,
@@ -10,13 +10,13 @@ CREATE table accounts(
     is_deleted bool DEFAULT false
 );
 
-CREATE table learners(
+CREATE table learners (
     account_id uuid PRIMARY KEY,
-    school_id uuid NOT NULL
+    school_id uuid UNIQUE NOT NULL
 );
 
 CREATE table staffs (
     account_id uuid PRIMARY KEY,
-    role smallint NOT NULL CHECK ( role in (1, 2) ) ,
+    role smallint NOT NULL CHECK (role in (1, 2)),
     created_by uuid NOT NULL
 );
--- internal/db/migrations/000003_create_table_schools.up.sql
+++ internal/db/migrations/000003_create_table_schools.up.sql
@@ -2,5 +2,5 @@ CREATE table schools(
     id uuid PRIMARY KEY,
     name character varying(250) NOT NULL,
     address character varying(250) NOT NULL,
-    created_by uuid NOT NULL
+    created_by uuid
 )
