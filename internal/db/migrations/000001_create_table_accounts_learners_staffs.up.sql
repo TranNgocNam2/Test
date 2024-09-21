0f6e3d71181b8f5b03e484be71e8fbb0ae13ae8a
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2024-09-21 10:15:04 +0700

    🧐 change data

--- internal/db/migrations/000001_create_table_accounts_learners_staffs.up.sql
+++ internal/db/migrations/000001_create_table_accounts_learners_staffs.up.sql
@@ -1,7 +1,7 @@
 CREATE table accounts (
-    id uuid PRIMARY KEY,
-    first_name character varying (10) NOT NULL,
-    last_name character varying(10) NOT NULL,
+    id varchar(20) PRIMARY KEY,
+    first_name character varying (20) NOT NULL,
+    last_name character varying(20) NOT NULL,
     email varchar(20) UNIQUE NOT NULL,
     phone varchar(10) UNIQUE NOT NULL,
     gender int,
@@ -11,12 +11,12 @@ CREATE table accounts (
 );
 
 CREATE table learners (
-    account_id uuid PRIMARY KEY,
+    account_id varchar(20) PRIMARY KEY,
     school_id uuid UNIQUE NOT NULL
 );
 
 CREATE table staffs (
-    account_id uuid PRIMARY KEY,
+    account_id varchar(20) PRIMARY KEY,
     role smallint NOT NULL CHECK (role in (1, 2)),
-    created_by uuid NOT NULL
+    created_by varchar(20) NOT NULL
 );
