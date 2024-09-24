5f28f3a7531563e2673d880a72145164e6d722e6
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2024-09-24 20:45:38 +0700

    💩 fix dockerfile

--- internal/validate/errors.go
+++ internal/validate/errors.go
@@ -1,9 +1,28 @@
 package validate
 
+import "encoding/json"
+
 type FieldError struct {
 	Field string `json:"field"`
 	Err   string `json:"error"`
 }
 
+func NewFieldsError(field string, err error) error {
+	return FieldErrors{
+		{
+			Field: field,
+			Err:   err.Error(),
+		},
+	}
+}
+
 // FieldErrors represents a collection of field errors.
 type FieldErrors []FieldError
+
+func (fe FieldErrors) Error() string {
+	d, err := json.Marshal(fe)
+	if err != nil {
+		return err.Error()
+	}
+	return string(d)
+}
